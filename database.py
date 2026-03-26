b277ea92ce009e054e8f8cfc355c0cd95227273e
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-26 13:37:39 +0530

    Initial Commit

--- database.py
+++ database.py
@@ -1,1 +1,25 @@
 import sqlite3
+
+
+def init_db():
+
+    conn = sqlite3.connect("shortener.db")
+    cursor = conn.cursor()
+
+    sql_query = """CREATE TABLE IF NOT EXISTS URLS(
+    
+    id INTEGER PRIMARY KEY,
+    url TEXT,
+    short_code TEXT,
+    click_count INTEGER DEFAULT 0
+ 
+    
+    );"""
+
+    cursor.execute(sql_query)
+
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    init_db()
