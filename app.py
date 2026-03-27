d72406c2f30bb71a096ccc32d1819955218b47eb
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-27 13:44:37 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask , requests 
+from flask import Flask , request , redirect , url_for , abort , jsonify
+from database import save_url
+from database import get_url
+from database import get_stats
 import string
 import secrets
 
+app = Flask(__name__)
+
+@app.route('/shorten', methods= ["POST"])
+def shorten():
+
+    data = request.get_json()
+
+    if not data or "url" not in data:
+        return {"error": "Ivalid Request"}, 400
+
+    url = data["url"]
+
+    if not url:
+        return ({"error": "Invalid JSON or missing Content-Type header"}), 400
+    
+    short_code = generate_id()
+    save_url(url, short_code)
+
+    return ({"short_url": short_code}), 201
+    
+    
 
 def generate_id():
 
@@ -12,9 +36,32 @@ def generate_id():
     return secure_string
 
 
+@app.route("/<short_code>" , methods = ["GET"])
+def redirect_url(short_code):
+    
+    result = get_url(short_code)
 
+    if result:
+        return redirect(result[0])
+    
+    else:
+        return  abort(404, description="Short URL not found")
+    
+@app.route("/stats/<short_code>", methods = ["GET"])
+def display_stats(short_code):
 
-app = Flask(__name__)
+    result = get_stats(short_code)
+
+    if result:
+        return jsonify({
+            "short_code" : f"http://127.0.0.1:5000/{short_code}",
+            "original_url": result[0],
+            "clicks":result[1]
+        }), 200
+    else:
+        return abort(404, description="Short URL not found")
+        
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -21,5 +21,47 @@ def init_db():
     conn.commit()
     conn.close()
 
+def save_url(original_url , short_code):
+    conn = sqlite3.connect("shortener.db")
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO urls (url, short_code) VALUES(?, ?)", (original_url, short_code))
+
+    conn.commit()
+    conn.close()
+
+def get_url(short_code):
+    
+    
+    with sqlite3.connect('shortener.db') as conn:
+    
+    
+        cursor = conn.cursor()
+
+        
+        cursor.execute("SELECT url from urls WHERE short_code = ?", (short_code,))
+
+        result = cursor.fetchone()
+
+        if result:
+            cursor.execute("UPDATE urls SET click_count = click_count + 1 WHERE short_code = ?", (short_code,))
+            return result
+        
+        return None
+    
+def get_stats(short_code):
+
+    with sqlite3.connect("shortener.db") as conn:
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT url, click_count from urls WHERE short_code = ?", (short_code,))
+        result = cursor.fetchone()
+
+        if result:
+            return result
+        
+        
+
+
 if __name__ == "__main__":
     init_db()
