0e0a7691f685bb5af7680efa8d845280cbc70f54
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-28 20:54:20 +0530

    fix: buggy font

--- app.py
+++ app.py
@@ -66,6 +66,10 @@ def display_stats(short_code):
 def home():
     return render_template("index.html")
 
+@app.route("/health")
+def health():
+    return {"status": "ok"}, 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
